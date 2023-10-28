3ee641db2401f76a0e1b1093ce4db8b9badab8d3
Bmg09 <git015.bikram.ghosh@gnkhalsa.edu.in>
2023-10-28 16:52:48 +0530

    Restructure finalized

--- src/restructFolderStructure.js
+++ src/restructFolderStructure.js
@@ -1,53 +1,61 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs') 
+const path = require('path') 
+const mime = require('mime') 
+const chalk = require('chalk')
 
 function getAllFilesInDirectory(dirPath, excludeFolderName = '') {
-    const files = [];
+    const files = [] 
 
     function collectFilesInDirectory(directory) {
-        const items = fs.readdirSync(directory);
+        const items = fs.readdirSync(directory) 
 
         for (const item of items) {
-            const itemPath = path.join(directory, item);
-            const stats = fs.statSync(itemPath);
+            const itemPath = path.join(directory, item) 
+            const stats = fs.statSync(itemPath) 
 
             if (stats.isDirectory()) {
                 if (!item.startsWith('.') && item !== excludeFolderName) {
-                    collectFilesInDirectory(itemPath);
+                    collectFilesInDirectory(itemPath) 
                 }
             } else {
-                files.push(itemPath);
+                files.push(itemPath) 
             }
         }
     }
 
-    collectFilesInDirectory(dirPath);
-    return files;
+    collectFilesInDirectory(dirPath) 
+    return files 
 }
 
 function categorizeAndMoveFiles(dirPath, excludeFolderName = '', extension = []) {
-    const filesArray = getAllFilesInDirectory(dirPath, excludeFolderName);
+    const filesArray = getAllFilesInDirectory(dirPath, excludeFolderName) 
+    const filteredFilesArray = filesArray.filter((filePath) => {
+        const mimeType = mime.getType(filePath) 
+        const extensionFromMime = mime.getExtension(mimeType) 
 
-    for (const filePath of filesArray) {
-        const fileExtension = path.extname(filePath).toLowerCase();
-
-        let destinationFolder = path.join(dirPath, 'Other'); // Default to 'Other' folder
-
-        // Check if the file extension is in the extension array
-        if (extension.includes(fileExtension)) {
-            destinationFolder = path.join(dirPath, fileExtension.slice(1).toUpperCase());
+        return extension.includes(extensionFromMime) 
+    }) 
+    for (const filePath of filteredFilesArray) {
+        const mimeType = mime.getType(filePath)
+        const extensionFromMime = mime.getExtension(mimeType)
+        let destinationFolder = path.join(dirPath, 'Other')
+    
+        if (extension.includes(extensionFromMime)) {
+            destinationFolder = path.join(dirPath, extensionFromMime.toUpperCase()) 
         }
 
         // Create the destination folder if it doesn't exist
         if (!fs.existsSync(destinationFolder)) {
-            fs.mkdirSync(destinationFolder);
+            fs.mkdirSync(destinationFolder) 
         }
+        // Log the move
+        console.log(`Moving ${chalk.red(filePath)} to ${chalk.green(destinationFolder)}`);
 
         // Move the file to the destination folder
-        fs.renameSync(filePath, path.join(destinationFolder, path.basename(filePath)));
+        fs.renameSync(filePath, path.join(destinationFolder, path.basename(filePath))) 
     }
 }
 
 module.exports = {
     categorizeAndMoveFiles
-};
+}
